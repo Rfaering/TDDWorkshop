c37827cbfbf2f758d5a2b9715e88c2e0245ebcd6
Rasmus Færing Larsen <rfaering@gmail.com>
2014-08-04 17:07:04 +0200

    Added Template

---
+++ BowlingScore/BowlingScoreService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingScore
+{
+    public class BowlingScoreService
+    {
+        public int CalculateScore()
+        {
+            return 0;
+        }
+    }
+}
---
+++ BowlingScoreTest/BowlingScoreServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BowlingScore;
+
+namespace BowlingScoreTest
+{
+    [TestClass]
+    public class BowlingScoreServiceTest
+    {
+        private BowlingScoreService _target;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _target = new BowlingScoreService();
+        }
+
+        [TestMethod]
+        public void GiveBowlingServiceWhenTestMethodCalledThenZeroReturned()
+        {
+            var result = _target.CalculateScore();
+            Assert.AreEqual(0, result);
+        }
+    }
+}
